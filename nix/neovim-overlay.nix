1c0c1406c1e5e72790432a7019e5b51e004a1674
Muhammad Hammam <warofzen1@proton.me>
2024-02-27 22:20:57 +0700

    Update neovim-overlay.nix

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -77,6 +77,7 @@ with final.pkgs.lib; let
     # (mkNvimPlugin inputs.wf-nvim "wf.nvim") # (example) keymap hints | https://github.com/Cassin01/wf.nvim
     # ^ bleeding-edge plugins from flake inputs
     which-key-nvim
+    gruvbox
   ];
 
   extraPackages = with pkgs; [
